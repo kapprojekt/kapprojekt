e8b532de100b34b9698f13c4cafa039c915afa8e
kysniw <kuba.winsyk.forgames@gmail.com>
2025-03-10 01:22:00 +0100

    Changed admin page

--- app/(cms)/admin/page.tsx
+++ app/(cms)/admin/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import Script from "next/script";
-
 export default function Admin() {
   return (
     <>
       <head>
         <link href="admin/config.yml" type="text/yaml" rel="cms-config-url" />
+        <script
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          async
+        ></script>
       </head>
-      <Script
-        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-        async
-      />
-      <Script
+      <script
         src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"
         async
-      />
+      ></script>
     </>
   );
 }
