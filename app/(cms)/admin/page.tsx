c537b99896194012732af659f00d37dea88c7cd9
kysniw <kuba.winsyk.forgames@gmail.com>
2025-03-10 02:39:25 +0100

    Still fixing issues

--- app/(cms)/admin/page.tsx
+++ app/(cms)/admin/page.tsx
@@ -1,21 +1,27 @@
 "use client";
 
-import Script from "next/script";
-
 export default function Admin() {
   return (
-    <>
-      <head>
-        <link href="admin/config.yml" type="text/yaml" rel="cms-config-url" />
-        <Script
-          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          async
-        />
-      </head>
-      <Script
-        src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"
-        async
-      />
-    </>
+    <html
+      lang="en"
+      dangerouslySetInnerHTML={{
+        __html: `
+        <!doctype html>
+  <head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
+   <title>Content Manager</title>
+  </head>
+  <body>
+   <!-- Dashboard script-->
+   <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+   <!-- netlify identity -->
+   <script is:inline src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+   <!-- Customize the netlify identity widget to refresh on logout preventing users from using the cms when not logged in and then seeing an error -->
+   <script is:inline>netlifyIdentity.on('logout', () => location.reload());</script>
+  </body>`,
+      }}
+    />
   );
 }
