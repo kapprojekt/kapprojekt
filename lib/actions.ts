9493059e2384ca7cad19e9c647523e104588f4ad
kysniw <kuba.winsyk.forgames@gmail.com>
2025-07-10 21:18:28 +0200

    Added contact info

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,67 +1,69 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export type FormState = {
   errors?: {
     fullName?: string[];
     email?: string[];
-	localisation?: string[];
-	investType?: string[];
+    localisation?: string[];
+    investType?: string[];
     message?: string[];
   };
   message?: string | null;
 };
 
 const FormSchema = z.object({
-	fullName: z.string({required_error: 'To pole jest wymagane.'}),
-	email: z.string({required_error: 'To pole jest wymagane.'})
-			.min(5, 'Za mało znaków.')
-			.email('Nieprawidłowy email.'),
-	localisation: z.string({required_error: 'To pole jest wymagane.'})
-			.min(3, 'Za mało znaków.'),
-	investType: z.string({invalid_type_error: 'Podano nieprawidłową wartość.'}),
-	message: z.string({required_error: 'To pole jest wymagane.'})
-			.min(5, 'Pole musi mieć od 5 do 500 znaków.')
-			.max(500, 'Pole musi mieć od 5 do 500 znaków.')
-})
+  fullName: z.string({ required_error: "To pole jest wymagane." }),
+  email: z
+    .string({ required_error: "To pole jest wymagane." })
+    .min(5, "Za mało znaków.")
+    .email("Nieprawidłowy email."),
+  localisation: z
+    .string({ required_error: "To pole jest wymagane." })
+    .min(3, "Za mało znaków."),
+  investType: z.string({ invalid_type_error: "Podano nieprawidłową wartość." }),
+  message: z
+    .string({ required_error: "To pole jest wymagane." })
+    .min(5, "Pole musi mieć od 5 do 500 znaków.")
+    .max(500, "Pole musi mieć od 5 do 500 znaków."),
+});
 
 export async function sendMessage(prevState: FormState, formData: FormData) {
-	const validatedFields = FormSchema.safeParse(
-		Object.fromEntries(formData.entries())
-	)
+  const validatedFields = FormSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
 
-	if (!validatedFields.success) {
-		return {
-			errors: validatedFields.error.flatten().fieldErrors,
-			message: "Uzupełnij wymagane pola!",
-    	};
-  	}
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Uzupełnij wymagane pola!",
+    };
+  }
 
-	const { fullName, email, localisation, investType, message } = validatedFields.data;
+  const { fullName, email, localisation, investType, message } =
+    validatedFields.data;
 
-	let infoMessage = ''
+  let infoMessage = "";
 
-		const res = await fetch("https://formsubmit.co/ajax/kapprojekt.service@gmail.com", {
-			method: "POST",
-			headers: {
-				'Content-Type': 'application/json',
-				'Accept': 'application/json'
-			},
-			body: JSON.stringify({
-				"Imię i nazwisko": fullName.toString(),
-				"Email": email.toString(),
-				"Wiadomość": message.toString(),
-				"Lokalizacja inwestycji": localisation.toString(),
-				"Rodzaj inwestycji": investType.toString()
-			})
-		})
+  const res = await fetch("https://formsubmit.co/ajax/kontakt@kapprojekt.pl", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      "Imię i nazwisko": fullName.toString(),
+      Email: email.toString(),
+      Wiadomość: message.toString(),
+      "Lokalizacja inwestycji": localisation.toString(),
+      "Rodzaj inwestycji": investType.toString(),
+    }),
+  });
 
-		const response = await res.json();
-		if(response.success)
-			infoMessage = "Wysłano wiadomość."
-		else
-			infoMessage = "Wystąpił błąd podczas wysyłania."
+  const response = await res.json();
+  if (response.success) infoMessage = "Wysłano wiadomość.";
+  else infoMessage = "Wystąpił błąd podczas wysyłania.";
 
-	return {
-		message: infoMessage
-	}
+  return {
+    message: infoMessage,
+  };
 }
